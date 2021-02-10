3d01a4fe04f1212a9e154baf655b50430c19e5a9
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-10 17:40:52 +0100

    Added image of cheems and Nic Cage. I worked on a grid, which is now shoen in the game. The head of Nic Cage now fits in the grid by clicking on the panels.

--- main.py
+++ main.py
@@ -1,32 +1,57 @@
-import pygame as pg
+import sys
+import pygame
 
-import sys, pygame
+
+def position_test(input_pos, test_dimension):
+    """
+
+    :param input_pos:
+    :param test_dimension:
+    :return:
+    """
+    if 0 <= input_pos <= test_dimension/3:
+        return 0
+    elif test_dimension/3 < input_pos <= 2*test_dimension/3:
+        return test_dimension/3
+    else:
+        return 2*test_dimension/3
+
+
 pygame.init()
 
-size = width, height = 900, 600
+im_width = 300
+im_height = 300
+
+size = width, height = 900, 900
 speed = [1, 1]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Nicholas Cage')
+pygame.display.set_caption('Tic-Tac-Toe')
 
-ball = pygame.image.load("head.png")
-ballrect = pygame.Rect(100, 100, 256, 256)
-mouse_flag=False
+nic_cage = pygame.image.load('head.png')
+nic_cage_rect = pygame.Rect(100, 100, im_width, im_height)
+cheems = pygame.image.load('cheems.png')
+cheems_rect = pygame.Rect(100, 100, im_width, im_height)
+
+mouse_flag = False
 
 while 1:
     pos = 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_flag=True
+            mouse_flag = True
             pos = pygame.mouse.get_pos()
-            ballrect = pygame.Rect(pos[0]-128, pos[1]-128, 256, 256)
+            nic_cage_rect = pygame.Rect(position_test(pos[0], width), position_test(pos[1], height), 256, 256)
     screen.fill(black)
-    font = pygame.font.SysFont(None, 24)
-    img = font.render('Click somewhere on the screen. DO IT!!11!', True, (100, 100, 100))
-    screen.blit(img, (20, 20))
+    pygame.draw.line(screen, (255, 0, 0), (0, height/3), (width, height/3))
+    pygame.draw.line(screen, (255, 0, 0), (0, 2*height/3), (width, 2*height/3))
+    pygame.draw.line(screen, (255, 0, 0), (width/3, 0), (width/3, height))
+    pygame.draw.line(screen, (255, 0, 0), (2*width/3, 0), (2*width/3, height))
+#    font = pygame.font.SysFont(None, 24)
+#    img = font.render('Click somewhere on the screen. DO IT!!11!', True, (100, 100, 100))
+#    screen.blit(img, (20, 20))
     if mouse_flag:
-        screen.blit(ball, ballrect)
-    pygame.display.update()
-
+        screen.blit(nic_cage, nic_cage_rect)
+    pygame.display.flip()
