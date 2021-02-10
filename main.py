01f30174703362dd440f398c944b0784caa5c750
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-10 23:51:37 +0100

    Almost got a functional version of a tic-tac-toe now. I added graphics for crosses and circles. I need to make sure only one graphic can be fitted in a cell of the tic-tac-toe grid.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import sys
+
 import pygame
+
+import numpy as np
+
+import random
 
 
 def position_test(input_pos, test_dimension):
@@ -9,15 +14,17 @@ def position_test(input_pos, test_dimension):
     :param test_dimension:
     :return:
     """
-    if 0 <= input_pos <= test_dimension/3:
+    if 0 <= input_pos <= test_dimension / 3:
         return 0
-    elif test_dimension/3 < input_pos <= 2*test_dimension/3:
-        return test_dimension/3
+    elif test_dimension / 3 < input_pos <= 2 * test_dimension / 3:
+        return test_dimension / 3
     else:
-        return 2*test_dimension/3
+        return 2 * test_dimension / 3
 
 
 pygame.init()
+
+player1_flag = True
 
 im_width = 300
 im_height = 300
@@ -26,32 +33,39 @@ size = width, height = 900, 900
 speed = [1, 1]
 black = 0, 0, 0
 
+outer_circle_radius = 140
+inner_circle_radius = 120
+
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Tic-Tac-Toe')
 
-nic_cage = pygame.image.load('head.png')
-nic_cage_rect = pygame.Rect(100, 100, im_width, im_height)
-cheems = pygame.image.load('cheems.png')
-cheems_rect = pygame.Rect(100, 100, im_width, im_height)
-
-mouse_flag = False
+screen.fill(black)
 
 while 1:
-    pos = 0
+    pygame.draw.line(screen, (255, 0, 0), (0, height / 3), (width, height / 3))
+    pygame.draw.line(screen, (255, 0, 0), (0, 2 * height / 3), (width, 2 * height / 3))
+    pygame.draw.line(screen, (255, 0, 0), (width / 3, 0), (width / 3, height))
+    pygame.draw.line(screen, (255, 0, 0), (2 * width / 3, 0), (2 * width / 3, height))
+    #    font = pygame.font.SysFont(None, 24)
+    #    img = font.render('Click somewhere on the screen. DO IT!!11!', True, (100, 100, 100))
+    #    screen.blit(img, (20, 20))
+    pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_flag = True
-            pos = pygame.mouse.get_pos()
-            nic_cage_rect = pygame.Rect(position_test(pos[0], width), position_test(pos[1], height), 256, 256)
-    screen.fill(black)
-    pygame.draw.line(screen, (255, 0, 0), (0, height/3), (width, height/3))
-    pygame.draw.line(screen, (255, 0, 0), (0, 2*height/3), (width, 2*height/3))
-    pygame.draw.line(screen, (255, 0, 0), (width/3, 0), (width/3, height))
-    pygame.draw.line(screen, (255, 0, 0), (2*width/3, 0), (2*width/3, height))
-#    font = pygame.font.SysFont(None, 24)
-#    img = font.render('Click somewhere on the screen. DO IT!!11!', True, (100, 100, 100))
-#    screen.blit(img, (20, 20))
-    if mouse_flag:
-        screen.blit(nic_cage, nic_cage_rect)
-    pygame.display.flip()
+            #nic_cage_rect = pygame.Rect(position_test(pos[0], width), position_test(pos[1], height), 256, 256)
+            line_start_x = position_test(pos[0], width)
+            line_start_y = position_test(pos[1], height)
+            if player1_flag:
+                pygame.draw.line(screen, (0, 255, 0), (line_start_x, line_start_y), (line_start_x + height/3, line_start_y + height/3))
+                pygame.draw.line(screen, (0, 255, 0), (line_start_x, line_start_y + height/3), (line_start_x + width/3, line_start_y))
+                player1_flag = False
+            else:
+                center = (line_start_x + width/6, line_start_y + height/6)
+                pygame.draw.circle(screen, (0, 0, 255), center, outer_circle_radius)
+                pygame.draw.circle(screen, black, center, inner_circle_radius)
+                player1_flag = True
+    pygame.display.update()
+    pygame.time.Clock().tick(5)
