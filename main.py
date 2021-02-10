13c350eaa1126d31da3f55d4fa12c0d4ce1d2b3b
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-10 13:54:35 +0100

    The face of NIc Cage now appears witht the first mouse click.

--- main.py
+++ main.py
@@ -12,18 +12,21 @@ pygame.display.set_caption('Nicholas Cage')
 
 ball = pygame.image.load("head.png")
 ballrect = pygame.Rect(100, 100, 256, 256)
+mouse_flag=False
 
 while 1:
     pos = 0
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_flag=True
             pos = pygame.mouse.get_pos()
             ballrect = pygame.Rect(pos[0]-128, pos[1]-128, 256, 256)
     screen.fill(black)
     font = pygame.font.SysFont(None, 24)
     img = font.render('Click somewhere on the screen. DO IT!!11!', True, (100, 100, 100))
     screen.blit(img, (20, 20))
-    screen.blit(ball, ballrect)
+    if mouse_flag:
+        screen.blit(ball, ballrect)
     pygame.display.update()
 
