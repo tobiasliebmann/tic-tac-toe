47511f55e3e3c4f90c70e6f1b2b82159011b1b2b
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-11 18:19:36 +0100

    The winning screen is now finished. The issue regarding the last cell before the game ends is still present.

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ import math
 import numpy as np
 
 
+def init_game_state():
+    """
+
+    :return:
+    """
+    screen.fill(black)
+    # Draw 3x3-grid for tic-tac-toe
+    pygame.draw.line(screen, (255, 255, 255), (0, height / 3), (width, height / 3))
+    pygame.draw.line(screen, (255, 255, 255), (0, 2 * height / 3), (width, 2 * height / 3))
+    pygame.draw.line(screen, (255, 255, 255), (width / 3, 0), (width / 3, height))
+    pygame.draw.line(screen, (255, 255, 255), (2 * width / 3, 0), (2 * width / 3, height))
+
 def game_over_screen(message):
     """
 
@@ -103,32 +115,24 @@ inner_circle_radius = width/8
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Tic-Tac-Toe')
 
-screen.fill(black)
-
 # pick a font you have and set its size
 my_font = pygame.font.SysFont("Comic Sans MS", 30)
 retry_button = my_font.render("Click here for new game.", True, (0, 255, 255))
+retry_button_rect = retry_button.get_rect()
 
-# Draw 3x3-grid for tic-tac-toe
-pygame.draw.line(screen, (255, 255, 255), (0, height / 3), (width, height / 3))
-pygame.draw.line(screen, (255, 255, 255), (0, 2 * height / 3), (width, 2 * height / 3))
-pygame.draw.line(screen, (255, 255, 255), (width / 3, 0), (width / 3, height))
-pygame.draw.line(screen, (255, 255, 255), (2 * width / 3, 0), (2 * width / 3, height))
+init_game_state()
 
 while 1:
     pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            quit_game()
         if game_state == 1:
             if event.type == pygame.MOUSEBUTTONDOWN:
-
                 line_start_x = position_test(pos[0], width)
                 line_start_y = position_test(pos[1], height)
                 column_index = math.trunc(3*pos[0]/width)
                 row_index = math.trunc(3*pos[1]/height)
-
                 if player_turn % 2 != 0 and click_test(pos, state_matrix):
                     print("Im here.")
                     pygame.draw.line(screen, (0, 255, 0), (line_start_x, line_start_y), (line_start_x + height/3, line_start_y + height/3))
@@ -154,7 +158,7 @@ while 1:
                     player_turn = player_turn + 1
                     print(state_matrix)
                     print(player_turn)
-                elif player_turn >= 9:
+                elif player_turn >= 10:
                     game_state = 2
                     draw_flag = True
                     pygame.time.delay(1000)
@@ -163,11 +167,21 @@ while 1:
                     print("You cant place your marker here.")
         if game_state == 2:
             if player1_win_flag:
-                game_over_screen("X wins.")
+                game_over_screen("Cross wins.")
             elif player2_win_flag:
-                game_over_screen("o wins.")
+                game_over_screen("Circle wins.")
             elif draw_flag:
                 game_over_screen("Draw.")
-#        elif event.type == pygame.MOUSEBUTTONDOWN and game_state == 2:
-    pygame.display.update()
+            retry_button_graphic = screen.blit(retry_button, (100, 100))
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if retry_button_graphic.collidepoint(pos):
+                    print("Retry button was clicked.")
+                    game_state = 1
+                    player1_win_flag = False
+                    player2_win_flag = False
+                    draw_flag = False
+                    player_turn = 1
+                    state_matrix = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+                    init_game_state()
+    pygame.display.flip()
     pygame.time.delay(5)
