fede9149336cd27af63bb16a73423bf534ed40b3
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-02-12 17:15:30 +0100

    Continued working on the classes. I started a new main file called new_main.py which will be using the calsses.

--- state_machine.py
+++ state_machine.py
@@ -3,9 +3,16 @@ import numpy as np
 
 class State:
     # The current state of the system. There are only two states 1 and 2.
+    # todo: 0 - Corresponds to the tart screen.
     # 1 - Corresponds to the actual game.
-    # 2 - Is the ending screen.
+    # 2 - Player 1 has won.
+    # 3 - Player 2 has won.
+    # 4 - Game is a draw.
     state = 1
+
+    # Markers for the palyers that used in the state matrix.
+    player1_marker = 1
+    player2_marker = -1
 
     # The current turn in the tic-tac-toe game.
     turn = 1
@@ -76,34 +83,34 @@ class State:
         The checking for a winner is done by adding up the rows, columns or diagonals up.
         If the resulting value is 3 => player 1 has won.
         If the resulting values is -3 => player 2 has won.
-        :return: Returns 1 if player 1 is the winner. Returns -1 if player 2 is the winner.
+        The win flags are then changed accordingly.
+        :return: -
         """
         transposed_matrix = self.state_matrix.T
 
         # Check the rows for a winner
         for x in self.state_matrix:
             total = np.sum(x)
-
-            if total == 3:
-                return 1
-            if total == -3:
-                return -1
+            if total == 3*self.player1_marker:
+                self.player1_win_flag = True
+            if total == 3*self.player2_marker:
+                self.player2_win_flag = True
         # Check the columns for a winner
         for x in transposed_matrix:
             total = np.sum(x)
-            if total == 3:
-                return 1
-            if total == -3:
-                return -1
+            if total == 3*self.player1_marker:
+                self.player1_win_flag = True
+            if total == 3*self.player2_marker:
+                self.player2_win_flag = True
         # Checking the diagonals by looking at the trace of the original state_matrix and a transformed matrix where
         # the diagonal elements are taken from the bottom left corner to the top right corner of the state matrix.
         trafo_matrix = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
         trace = np.trace(self.state_matrix)
         trafo_trace = np.trace(np.matmul(trafo_matrix, self.state_matrix))
-        if trace == 3 or trafo_trace == 3:
-            return 1
-        if trace == -3 or trafo_trace == -3:
-            return -1
+        if trace == 3*self.player1_marker or trafo_trace == 3*self.player1_marker:
+            self.player1_win_flag = True
+        if trace == 3*self.player2_marker or trafo_trace == 3*self.player2_marker:
+            self.player2_win_flag = True
 
     def init_game_state(self):
         """
@@ -117,9 +124,26 @@ class State:
         self.player2_win_flag = False
         self.draw_flag = False
 
-    def on_click(self):
+    def on_click(self, row_index, column_index):
         """
         Defines the actions that are performed, when the screen is clicked and a new graphic is add to the tic-tac-toe
         grid.
         :return:
         """
+        if self.state_matrix[row_index][column_index] == 0:
+            if self.turn % 2 != 0:
+                self.set_state_matrix_component(row_index, column_index, self.player1_marker)
+            else:
+                self.set_state_matrix_component(row_index, column_index, self.player2_marker)
+            self.turn += 1
+            # You need at least 5 turns before you can win
+            if self.turn >= 6:
+                self.win_check()
+                if self.player1_win_flag:
+                    self.game_state = 2
+                elif self.player2_win_flag:
+                    self.game_state = 3
+                elif self.turn >= 10:
+                    self.game_state = 4
+        else:
+            print("You can not make this move.")
--- visualization.py
+++ visualization.py
@@ -1,11 +1,15 @@
 import pygame as pg
 
+import state_machine as sm
+
 
 class Graphics:
     screen_width = 900
     screen_height = screen_width
     screen_size = (screen_height, screen_width)
     screen = pg.display.set_mode(screen_size)
+
+    game_state = sm.State()
 
     cross_length = screen_width / 3
 
@@ -20,6 +24,7 @@ class Graphics:
 
     def __init__(self):
         pg.init()
+        self.game_state.init_game_state()
 
     def get_screen_size(self):
         """
@@ -67,45 +72,63 @@ class Graphics:
         """
         return self.screen_height
 
-    def init_grid(self):
+    def init_background(self):
         """
 
         :return:
         """
         pg.display.set_caption('Tic-Tac-Toe')
         self.screen.fill(self.black)
-        # Draw 3x3-grid for tic-tac-toe
-        pg.draw.line(self.screen, self.white, (0, self.screen_height / 3), (self.screen_width, self.screen_height / 3))
-        pg.draw.line(self.screen, self.white, (0, 2 * self.screen_height / 3),
-                     (self.screen_width, 2 * self.screen_height / 3))
-        pg.draw.line(self.screen, self.white, (self.screen_width / 3, 0), (self.screen_width / 3, self.screen_height))
-        pg.draw.line(self.screen, self.white, (2 * self.screen_width / 3, 0),
-                     (2 * self.screen_width / 3, self.screen_height))
+        if self.game_state.get_state() == 1:
+            # Draw 3x3-grid for tic-tac-toe
+            pg.draw.line(self.screen, self.white, (0, self.screen_height / 3), (self.screen_width, self.screen_height / 3))
+            pg.draw.line(self.screen, self.white, (0, 2 * self.screen_height / 3),
+                         (self.screen_width, 2 * self.screen_height / 3))
+            pg.draw.line(self.screen, self.white, (self.screen_width / 3, 0), (self.screen_width / 3, self.screen_height))
+            pg.draw.line(self.screen, self.white, (2 * self.screen_width / 3, 0),
+                         (2 * self.screen_width / 3, self.screen_height))
 
     def draw_cross(self, pos_x, pos_y):
         """
-        :param pos_x: x coordinate of the top left corner of a cross
-        :param pos_y: y coordinate of the top left corner of a cross
-        :return:
+        :param pos_x: x coordinate of the middle of the cross.
+        :param pos_y: y coordinate of the middle of the cross.
+        :return: -
         """
-        pg.draw.line(self.screen, self.green, (pos_x, pos_y), (pos_x + self.cross_length, pos_y + self.cross_length))
-        pg.draw.line(self.screen, self.green, (pos_x, pos_y + self.cross_length), (pos_x + self.cross_length, pos_y))
+        pg.draw.line(self.screen, self.green, (pos_x - self.cross_length/2, pos_y - self.cross_length/2),
+                     (pos_x + self.cross_length/2, pos_y + self.cross_length/2))
+        pg.draw.line(self.screen, self.green, (pos_x - self.cross_length/2, pos_y - self.cross_length/2),
+                     (pos_x + self.cross_length/2, pos_y + self.cross_length/2))
 
     def draw_circle(self, pos_x, pos_y):
         """
 
-        :param pos_x:
-        :param pos_y:
+        :param pos_x: x coordinate of the center of the circle.
+        :param pos_y: y coordinate of the center of the circle.
         :return:
         """
         pg.draw.circle(self.screen, self.blue, (pos_x, pos_y), self.outer_circle_radius)
         pg.draw.circle(self.screen, self.black, (pos_x, pos_y), self.inner_circle_radius)
 
-    def render_state_matrix(self, state_matrix):
+    def convert_indices_to_position(self, row_index, column_index):
         """
-
-        :param state_matrix
-        :return:
+        The function converts the indices of a state matrix into a position on the screen.
+        :param row_index: row index of a state matrix. Possible values are 0,1 or 2
+        :param column_index: column index of a state matrix. Possible values are 0,1 or 2
+        :return: The according position on the screen
         """
+        return (self.screen_width * row_index/3 + self.screen_width/6,
+                self.screen_height * column_index/3 + self.screen_height/6)
 
-
+    def render_game(self):
+        """
+        This method
+        :return: -
+        """
+        state_matrix = self.game_state.get_state_matrix()
+        for row_index in range(3):
+            for column_index in range(3):
+                (x, y) = self.convert_indices_to_position(row_index, column_index)
+                if state_matrix[row_index][column_index] == self.game_state.player1_marker:
+                    self.draw_cross(x, y)
+                elif state_matrix[row_index][column_index] == self.game_state.player1_marker:
+                    self.draw_circle(x, y)
